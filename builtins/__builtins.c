c561850e22ebbf50c12749b046c7d5271cbc22da
Antoine FEUERSTEIN <afeuerst@e2r9p2.42.fr>
2017-02-26 17:55:52 +0100

    fixing execute any binary

--- builtins/__builtins.c
+++ builtins/__builtins.c
@@ -6,7 +6,7 @@
 /*   By: afeuerst <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/26 15:23:20 by afeuerst          #+#    #+#             */
-/*   Updated: 2017/02/26 17:05:11 by afeuerst         ###   ########.fr       */
+/*   Updated: 2017/02/26 17:54:35 by afeuerst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,23 @@ static void				ft_exit_destructor(const t_cmd *cmd)
 {
 	(void)cmd;
 	exit(0);
+}
+
+static void				ft_local_exe(const t_cmd * const cmd)
+{
+	pid_t				pid;
+	extern char			**environ;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(*cmd->args, cmd->args, environ);
+		write(1, *cmd->args, ft_strlen(*cmd->args));
+		wwrite(1, " No such file or directory\n")
+		exit(1);
+	}
+	else
+		wait(NULL);
 }
 
 uint32_t				ft_builtins(const t_cmd * const cmd)
@@ -32,9 +49,9 @@ uint32_t				ft_builtins(const t_cmd * const cmd)
 	}
 	else if (!ft_strncmp(*cmd->args, "exit", 4))
 		ft_exit_destructor(cmd);
-	else if (!ft_strncmp(cmd->copy, "./", 2))
+	else if (!ft_strncmp(cmd->copy, "./", 2) || !ft_strncmp(cmd->copy, "/", 1))
 	{
-		ft_start_command(cmd);
+		ft_local_exe(cmd);
 		return (0);
 	}
 	else if (!ft_strncmp(*cmd->args, "shell", 5))
